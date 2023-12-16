5c7249b7b856c294a830e66eb9a1f1bf69bab2d9
Meriç Kerem Yalçın <merickereme@gmail.com>
2023-12-16 16:24:59 +0300

    game ending condition checked, game record request created

--- src/main/java/edu/odtu/ceng453/group10/catanfrontend/ui/GameClient.java
+++ src/main/java/edu/odtu/ceng453/group10/catanfrontend/ui/GameClient.java
@@ -2,9 +2,14 @@ package edu.odtu.ceng453.group10.catanfrontend.ui;
 
 import edu.odtu.ceng453.group10.catanfrontend.config.Settings;
 import edu.odtu.ceng453.group10.catanfrontend.game.GameState;
+import edu.odtu.ceng453.group10.catanfrontend.game.Player;
 import edu.odtu.ceng453.group10.catanfrontend.requests.LoginResponse;
+import edu.odtu.ceng453.group10.catanfrontend.requests.Request;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
@@ -47,6 +52,17 @@ public class GameClient {
   }
 
   public Scene getGameScene(Stage gameStage) {
+    Player winnerPlayer = checkWinCondition();
+    if(winnerPlayer != null) {
+      Request request = new Request();
+      request.saveGameResult(winnerPlayer.getName(), winnerPlayer.getPoints());
+      Alert alert = new Alert(Alert.AlertType.INFORMATION);
+      alert.setTitle("Game Over");
+      alert.setContentText("Winner is " + winnerPlayer.getName() + " with " + winnerPlayer.getPoints() + " points!");
+      alert.showAndWait();
+      gameStage.close();
+      System.exit(0);
+    }
     BorderPane totalPane = new BorderPane();
     BorderPane.setAlignment(boardView, Pos.CENTER);
     totalPane.setCenter(boardView);
@@ -116,6 +132,24 @@ public class GameClient {
     });
     escPane.add(quitButton, 0, 0);
     return new Scene(escPane, Settings.getWidth(), Settings.getHeight());
+  }
+
+  private Player checkWinCondition() {
+    List<Player> players = state.getPlayers();
+    Player winnerPlayer = null;
+    for(Player player : players) {
+      if(player.getPoints() >= 8) {
+        if(winnerPlayer == null) {
+          winnerPlayer = player;
+        }
+        else {
+          if(player.getPoints() > winnerPlayer.getPoints()) {
+            winnerPlayer = player;
+          }
+        }
+      }
+    }
+    return winnerPlayer;
   }
 
 
